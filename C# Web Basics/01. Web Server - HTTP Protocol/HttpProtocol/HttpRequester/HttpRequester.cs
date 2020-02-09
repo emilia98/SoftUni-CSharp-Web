83902e3d74e441bc44b49d129ad1a50188f2c417
unknown <emilia_nedialkova@abv.bg>
2020-02-10 00:05:23 +0200

    added dmo for HTTP Protocol

---
+++ C# Web Basics/01. Web Server - HTTP Protocol/HttpProtocol/HttpRequester/HttpRequester.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpRequester
+{
+    class HttpRequester
+    {
+        static async Task Main()
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync("https://softuni.bg");
+            string result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ C# Web Basics/01. Web Server - HTTP Protocol/HttpProtocol/HttpRequester_Save/HttpRequester.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpRequester_Save
+{
+    class HttpRequester
+    {
+        static async Task Main()
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync("https://softuni.bg");
+            string result = await response.Content.ReadAsStringAsync();
+            await File.WriteAllTextAsync("index.html", result);
+        }
+    }
+}
