e58fe1e0ffa75d049b2b7a9cc0da158429e1ed20
unknown <emilia_nedialkova@abv.bg>
2020-02-10 22:32:24 +0200

    added async processing to web server

--- C# Web Basics/SoftUni-Information-Services/SIS-v2/BasicServer-Async/StartUp.cs
+++ C# Web Basics/SoftUni-Information-Services/SIS-v2/BasicServer-Async/StartUp.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BasicServer_Async
@@ -10,44 +11,50 @@ namespace BasicServer_Async
     {
         static async Task Main()
         {
-            const string NewLine = "\r\n";
-
             TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 1234);
             tcpListener.Start();
 
             while (true)
             {
-                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+                await Task.Run(() => ProcessClientAsync(tcpClient));
+            }
+        }
 
-                using (NetworkStream networkStream = tcpClient.GetStream())
-                {
-                    // TODO: Use buffer
-                    byte[] requestBytes = new byte[1000000];
-                    int bytesRead = networkStream.Read(requestBytes, 0, requestBytes.Length);
-                    string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
+        private static async Task ProcessClientAsync(TcpClient tcpClient)
+        {
+            const string NewLine = "\r\n";
 
-                    string responseText = "<h1>Hello Header</h1>";
-                    string responseForm = @"<form action='/Account/Login' method='post'> 
+            using (NetworkStream networkStream = tcpClient.GetStream())
+            {
+                // TODO: Use buffer
+                byte[] requestBytes = new byte[1000000];
+                int bytesRead = await networkStream.ReadAsync(requestBytes, 0, requestBytes.Length);
+                string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
+
+                string responseText = "<h1>Hello Header</h1>";
+                string responseForm = @"<form action='/Account/Login' method='post'> 
                                                 <input type='date' name='date' />
                                                 <input type='text' name='username' />
                                                 <input type='password' name='password' />
                                                 <input type='submit' value='Login'/>
-                                            </form>";
-                    string response =
-                                    "HTTP/1.0 200 OK" + NewLine
-                                    // "HTTP/1.0 307 OK" + NewLine
-                                    + "Server: SoftUniServer/1.0" + NewLine
-                                    // + "Content-Disposition: attachment; filename=emilia.html" + NewLine
-                                    // + "Location: https://google.com" + NewLine
-                                    + "Content-Type: text/html" + NewLine
-                                    + $"Content-Length: {responseText.Length + responseForm.Length}" + NewLine
-                                    + NewLine
-                                    + responseText + responseForm;
-                    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
-                    networkStream.Write(responseBytes, 0, responseBytes.Length);
-                    Console.WriteLine(request);
-                    Console.WriteLine(new string('=', 60));
-                }
+                                            </form>"
+                                        + "<p>" + DateTime.UtcNow + "</p>";
+                Thread.Sleep(10000);
+                string response =
+                                "HTTP/1.0 200 OK" + NewLine
+                                // "HTTP/1.0 307 OK" + NewLine
+                                + "Server: SoftUniServer/1.0" + NewLine
+                                // + "Content-Disposition: attachment; filename=emilia.html" + NewLine
+                                // + "Location: https://google.com" + NewLine
+                                + "Content-Type: text/html" + NewLine
+                                + $"Content-Length: {responseText.Length + responseForm.Length}" + NewLine
+                                + NewLine
+                                + responseText + responseForm;
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                await networkStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+                Console.WriteLine(request);
+                Console.WriteLine(new string('=', 60));
             }
         }
     }
