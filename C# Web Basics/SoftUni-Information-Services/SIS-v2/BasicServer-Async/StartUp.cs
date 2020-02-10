8a0935880a0eb0ff2542f9dada678261136af08d
unknown <emilia_nedialkova@abv.bg>
2020-02-10 21:23:39 +0200

    init SIS

---
+++ C# Web Basics/SoftUni-Information-Services/SIS-v2/BasicServer-Async/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicServer_Async
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            const string NewLine = "\r\n";
+
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 1234);
+            tcpListener.Start();
+
+            while (true)
+            {
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+
+                using (NetworkStream networkStream = tcpClient.GetStream())
+                {
+                    // TODO: Use buffer
+                    byte[] requestBytes = new byte[1000000];
+                    int bytesRead = networkStream.Read(requestBytes, 0, requestBytes.Length);
+                    string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
+
+                    string responseText = "<h1>Hello Header</h1>";
+                    string responseForm = @"<form action='/Account/Login' method='post'> 
+                                                <input type='date' name='date' />
+                                                <input type='text' name='username' />
+                                                <input type='password' name='password' />
+                                                <input type='submit' value='Login'/>
+                                            </form>";
+                    string response =
+                                    "HTTP/1.0 200 OK" + NewLine
+                                    // "HTTP/1.0 307 OK" + NewLine
+                                    + "Server: SoftUniServer/1.0" + NewLine
+                                    // + "Content-Disposition: attachment; filename=emilia.html" + NewLine
+                                    // + "Location: https://google.com" + NewLine
+                                    + "Content-Type: text/html" + NewLine
+                                    + $"Content-Length: {responseText.Length + responseForm.Length}" + NewLine
+                                    + NewLine
+                                    + responseText + responseForm;
+                    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                    networkStream.Write(responseBytes, 0, responseBytes.Length);
+                    Console.WriteLine(request);
+                    Console.WriteLine(new string('=', 60));
+                }
+            }
+        }
+    }
+}
