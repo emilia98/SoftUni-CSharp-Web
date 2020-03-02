98d7ce491ada32e0d9e4b91ece1be53f2d16870c
unknown <emilia_nedialkova@abv.bg>
2020-03-03 01:24:44 +0200

    first demo for ASP NET Core Course

---
+++ ASP.NET Core/01. ASP NET Core Introduction/AspNetCoreIntroDemo/AspNetCoreIntroMVC/Controllers/InfoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreIntroMVC.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Json(string username)
+        {
+            if(!String.IsNullOrEmpty(username)) 
+            {
+                return this.Json(new { Username = username });
+            }
+
+            return this.View();
+        }
+
+        public IActionResult PhysicalFile()
+        {
+            string path = @"C:\Users\Emilia\Desktop\bulgaria_flag.png";
+            return this.PhysicalFile(path, "image/png");
+        }
+
+        public IActionResult Redirect()
+        {
+            return this.RedirectToAction("Home", "Home");
+        }
+    }
+}
