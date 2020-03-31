ea109f77f4c9c7db70ef67eb6a020f219f2f1d9e
unknown <emilia_nedialkova@abv.bg>
2020-04-01 00:10:53 +0300

    started COnsultation Day Demo (01.03)

---
+++ ASP.NET Core/05. Consultation Day/ConsultationDay/DemoApp/Areas/Administration/Controllers/PaymentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApp.Areas.Administration.Controllers
+{
+    [Area("Administration")]
+    public class PaymentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
