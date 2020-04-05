dcac185b0dd5711109842f8156cf3c30fcae521e
unknown <emilia_nedialkova@abv.bg>
2020-04-05 20:13:12 +0300

    init WebApi Demo

---
+++ ASP.NET Core/07. Web API/WebApi/DemoApp/Controllers/TheoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class TheoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult XhrRequest()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.NET Core/07. Web API/WebApi/DemoApp/Controllers/XhrRequestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class XhrRequestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult JsonTest()
+        {
+            return this.Json(new { FirstName = "Niki", LastName = "Kostov" });
+        }
+
+        public IActionResult Xhr()
+        {
+            return View();
+        }
+
+        public IActionResult ParseJson()
+        {
+            return View();
+        }
+    }
+}
